5b7d762af61df10002a0c787ade04deeae8327dd
Daniel Silva <daniel@visualyst.co>
2021-02-16 23:24:58 +0100

    Working dynamic navigation using JSON data

--- ares-nettside/src/Components/Nav.js
+++ ares-nettside/src/Components/Nav.js
@@ -4,32 +4,42 @@ import UserStore from '../Stores/UserStore';
 import PagesJson from '../JsonData/pages.json';
 
 const isLoggedIn = UserStore.isLoggedIn;
+const { pages } = PagesJson;
 
 class Nav extends React.Component {
     render() {
 
-        //const { pages } = PagesJson;
-
         if(isLoggedIn) {
             return (
-                <Nav id="adminNav">
+                <nav id="adminNav">
                     <ul>
-                        {/*
-                            Object.keys(pages).map((e, i) => {
-                                <Link to={e.route} className="adminLink"><li>{e.title}</li></Link>
+                        {
+                            pages.map(function(page, index){
+                                return (
+                                    <Link to={page.route} className="adminLink"><li>{page.title}</li></Link>
+                                );
                             })
-                        */}
-                        {/*arr.map(item => <Link to={item.route} className="adminLink"><li>{item.title}</li></Link>)*/}
+                        }
                     </ul>
-                </Nav>
+                </nav>
             );
         } else {
             return (
-                <Nav>
+
+                <nav>
                     <ul>
-                        {/*arr.map(item => <Link to={item.route}><li>{item.title}</li></Link>)*/}
+                        {
+                            pages.map(function(page, index){
+                                if(!page.private) {
+                                    return (
+                                        <Link to={page.route}><li>{page.title}</li></Link>
+                                    );
+                                }
+                            })
+                        }
                     </ul>
-                </Nav>
+                </nav>
+    
             );
         }
 
