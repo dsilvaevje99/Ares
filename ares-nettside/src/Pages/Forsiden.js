0bd084f5b0287b186b6baf72fc918fcd2a83bfe8
Daniel Silva <daniel@visualyst.co>
2021-02-18 15:10:22 +0100

    Setup working text editor with CKeditor

--- ares-nettside/src/Pages/Forsiden.js
+++ ares-nettside/src/Pages/Forsiden.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import TextEditor from '../Components/TextEditor';
 
 const Forsiden = () => {
     return (
         <div className="pageContent">
-            <h1>Home page</h1>
+            <h1>Forsiden</h1>
+            <TextEditor/>
         </div>
     );
 };
